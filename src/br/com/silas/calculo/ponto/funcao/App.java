ff8e464b45682aba18ca069768a69d959e8c11dc
Silas <silasdjd@gmail.com>
2017-08-15 01:03:18 -0300

    Primeiro Commmit

---
+++ src/br/com/silas/calculo/ponto/funcao/App.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.silas.calculo.ponto.funcao;
+
+import br.com.silas.calculo.ponto.funcao.calculos.Complexidade;
+
+/**
+ *
+ * @author Silas
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Complexidade ALTA descricao: " + Complexidade.ALTA.toString());
+        System.out.println("Complexidade ALTA ordem: " + Complexidade.ALTA.ordinal());
+    }
+    
+}
---
+++ src/br/com/silas/calculo/ponto/funcao/calculos/EntradaComplexidade.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.silas.calculo.ponto.funcao.calculos;
+
+/**
+ *
+ * @author Silas
+ */
+public class EntradaComplexidade {
+
+    private Range rangeX;
+    private Range rangeY;
+    private Complexidade complexidade;
+
+    public EntradaComplexidade(Complexidade complexidade, Range rangeX, Range rangeY) {
+        if (rangeX == null || rangeY == null) {
+            throw new IllegalArgumentException("O preenchimento dos ranges é obrigatório. Argumentos inválidos");
+        }
+
+        if (complexidade == null) {
+            throw new IllegalArgumentException("O preenchimento da complexidade é obrigatório. Argumentos inválidos");
+        }
+
+        this.complexidade = complexidade;
+        this.rangeX = rangeX;
+        this.rangeY = rangeY;
+    }
+
+    public boolean avaliar(Integer qtdX, Integer qtdY) {
+        return rangeX.avaliar(qtdX) && rangeY.avaliar(qtdY);
+    }
+
+    @Override
+    public String toString() {
+        return "EntradaComplexidade{" + "rangeX=" + rangeX + ", rangeY=" + rangeY + '}';
+    }
+
+    public Complexidade getComplexidade() {
+        return this.complexidade;
+    }
+
+}
