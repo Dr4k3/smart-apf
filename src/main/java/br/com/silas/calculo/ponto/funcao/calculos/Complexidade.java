35c67d0795f37e0c094031d907700e6a59cd5fe6
Fabio Oliveira <fabio.bonnie@gmail.com>
2017-11-07 22:12:28 -0200

    Adicionando configurações do Maven e Eclipse

---
+++ src/main/java/br/com/silas/calculo/ponto/funcao/calculos/Complexidade.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.silas.calculo.ponto.funcao.calculos;
+
+/**
+ *
+ * @author Silas
+ */
+public enum Complexidade {
+    
+    BAIXA, 
+    MEDIA, 
+    ALTA,
+    INDETERMINADA;
+    
+    
+}
---
+++ src/main/java/br/com/silas/calculo/ponto/funcao/calculos/Range.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.silas.calculo.ponto.funcao.calculos;
+
+/**
+ * @author Silas
+ */
+public class Range  {
+    
+    private Integer max;
+    private Integer min;
+    
+    public Range(Integer min, Integer max){
+        this.min = min;
+        this.max = max;
+    }
+    
+    public boolean avaliar(Integer number) {
+        return number.compareTo(min)>=0 && number.compareTo(max)<=0;
+    }
+    
+    public static void main(String []args){
+        System.out.println(new Range(1, 10).avaliar(10));
+        System.out.println(new Range(1, 10).avaliar(1));
+        System.out.println(new Range(1, 10).avaliar(11));
+    }
+
+    @Override
+    public String toString() {
+        return "Range{" + "min=" + min + ", max=" + max + '}';
+    }
+    
+    
+    
+}
